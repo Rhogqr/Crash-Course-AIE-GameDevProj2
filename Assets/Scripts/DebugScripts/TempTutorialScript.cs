e190c89a81fc9871c9098291f883388bccf916bd
Rhogar <reuben@coreproductions.com.au>
2025-10-30 12:33:15 +1100

    Half of Tutorial Done

--- Assets/Scripts/DebugScripts/TempTutorialScript.cs
+++ Assets/Scripts/DebugScripts/TempTutorialScript.cs
@@ -6,13 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class TempTutorialScript : MonoBehaviour
 {
-    PlayerStatsManager pSM;
+    [SerializeField] string playerDirection;
     [SerializeField] GameObject[] tutorialScreens;
     string tempDirection = "";
+    public GameObject mainPlayerCam;
+
+    [SerializeField] GameObject HealthBar;
+    [SerializeField] GameObject SineWave;
+    [SerializeField] GameObject SineWaveAlt;
+    //[SerializeField] GameObject OxygenPump;
+    [SerializeField] GameObject EyeAlien;
+    [SerializeField] GameObject TaskAlien;
+
     // Start is called before the first frame update
     void Start()
     {
-        pSM = GameObject.Find("PlayerStatsManager").GetComponent<PlayerStatsManager>();
+        mainPlayerCam = GameObject.Find("Main Camera");
+        HealthBar.SetActive(false);
+        SineWave.SetActive(false);
+        SineWaveAlt.SetActive(false);
+        //OxygenPump.SetActive(false);
+        EyeAlien.SetActive(false);
+        TaskAlien.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,28 +38,75 @@ public class TempTutorialScript : MonoBehaviour
             SceneManager.LoadScene("PrototypeStartScreen");
         }
 
+        transform.rotation = mainPlayerCam.transform.rotation;
 
-        if (tempDirection != pSM.playerDirection)
+        //if (tempDirection != playerDirection)
+        //{
+        //    switch (tempDirection)
+        //    {
+        //        case "Forward":
+        //            tutorialScreens[0].SetActive(false);
+        //            break;
+        //        case "Left":
+        //            tutorialScreens[1].SetActive(false);
+        //            break;
+        //        case "Right":
+        //            tutorialScreens[2].SetActive(false);
+        //            break;
+        //        case "Backward":
+        //            tutorialScreens[3].SetActive(false);
+        //            break;
+
+        //    }
+        //}
+        //tempDirection = playerDirection;
+
+        if (playerDirection == "Forward")
         {
-            switch (tempDirection)
-            {
-                case "Forward":
-                    tutorialScreens[0].SetActive(false);
-                    break;
-                case "Left":
-                    tutorialScreens[1].SetActive(false);
-                    break;
-                case "Right":
-                    tutorialScreens[2].SetActive(false);
-                    break;
-                case "Backward":
-                    tutorialScreens[3].SetActive(false);
-                    break;
-
-            }
+            tutorialScreens[0].SetActive(true);
+            //if (OxygenPump != null)
+            //OxygenPump.SetActive(true);
         }
-        tempDirection = pSM.playerDirection;
+        else
+        {
+            tutorialScreens[0].SetActive(false);
+            //OxygenPump.SetActive(false);
+        }
+        if (playerDirection == "Left")
+        {
+            tutorialScreens[1].SetActive(true);
+            HealthBar.SetActive(true);
+            EyeAlien.SetActive(true);
+        }
+        else
+        {
+            tutorialScreens[1].SetActive(false);
+            HealthBar.SetActive(false);
+            EyeAlien.SetActive(false);
+        }
+        if (playerDirection == "Right")
+        {
+            tutorialScreens[2].SetActive(true);
+            SineWave.SetActive(true);
+            SineWaveAlt.SetActive(true);
+            TaskAlien.SetActive(true);
+        }
+        else
+        {
+            tutorialScreens[2].SetActive(false);
+            SineWave.SetActive(false);
+            SineWaveAlt.SetActive(false);
+            TaskAlien.SetActive(false);
+        }
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Direction"))
+        {
+            playerDirection = other.name;
+        }
     }
 
 }
